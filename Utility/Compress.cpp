0f89525e5e17056820b91ab89ba46877409424a9
yuruhi <yuruhiya@gmail.com>
2020-05-05 11:18:57 +0900

    add

---
+++ Utility/Compress.cpp
@@ -0,0 +1,43 @@
+// shortcut : Compress
+// description : 座標圧縮
+template<class T>class Compress {
+	vector<T> v;
+	bool flag = false;
+public:
+	Compress() {}
+	template<class U>Compress(const U& a) { add(a); }
+	template<class U>Compress& operator<<(const U& a) { add(a); return *this; }
+	Compress& add(const T& a) {
+		flag = false;
+		v.push_back(a);
+		return *this;
+	}
+	Compress& add(const vector<T>& a) {
+		flag = false;
+		v.insert(v.end(), a.begin(), a.end());
+		return *this;
+	}
+	template<class U>Compress& add(const initializer_list<U>& a) {
+		flag = false;
+		for (const auto& v : a)add(v);
+		return *this;
+	}
+	void build() {
+		sort(v.begin(), v.end());
+		v.erase(unique(v.begin(), v.end()), v.end());
+		flag = true;
+	}
+	int operator()(const T& x)const {
+		assert(flag);
+		return lower_bound(v.begin(), v.end(), x) - v.begin();
+	}
+	vector<int> operator()(const vector<T>& x)const {
+		assert(flag);
+		vector<int> res; res.reserve(x.size());
+		for (const T& i : x)res.push_back(lower_bound(v.begin(), v.end(), i) - v.begin());
+		return res;
+	}
+	int size()const { assert(flag); return v.size(); }
+	const vector<T>& operator*()const { assert(flag); return v; }
+	const T& operator[](int i)const { assert(flag); return v[i]; }
+};
