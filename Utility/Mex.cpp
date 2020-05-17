c69eafe086978d246dbdcc61c3192b0e52bb0e07
yuruhi <yuruhiya@gmail.com>
2020-05-17 12:54:50 +0900

    add

---
+++ Utility/Mex.cpp
@@ -0,0 +1,10 @@
+// shortcut : Mex
+// description : a に含まれない最小の非負整数
+template<class T>int mex(const T& a) {
+	int x = 0;
+	for (int i : a) {
+		if (x < i)break;
+		if (i == x)x++;
+	}
+	return x;
+}
