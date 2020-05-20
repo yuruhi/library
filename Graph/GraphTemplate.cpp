a051498e0cd4bdf6b71b7761fb3fe9641443276b
yuruhi <yuruhiya@gmail.com>
2020-05-20 12:29:03 +0900

    fix

--- Graph/GraphTemplate.cpp
+++ Graph/GraphTemplate.cpp
@@ -6,15 +6,21 @@ struct Edge {
 	int to; Weight cost;
 	Edge() :to(-1), cost(-1) {}
 	Edge(int _to, Weight _cost = 1) :to(_to), cost(_cost) {}
-	friend bool operator>(const Edge& e1, const Edge& e2) { return e1.cost > e2.cost; }
-	friend ostream& operator<<(ostream& os, const Edge& e) { return os << "->" << e.to << '(' << e.cost << ')'; }
+	friend bool operator>(const Edge& e1, const Edge& e2) {
+		return e1.cost > e2.cost;
+	}
+	friend ostream& operator<<(ostream& os, const Edge& e) {
+		return os << "->" << e.to << '(' << e.cost << ')';
+	}
 };
 using Graph = vector<vector<Edge>>;
 struct Edge2 {
 	int from, to; Weight cost;
 	Edge2() :from(-1), to(-1), cost(0) {}
 	Edge2(int _from, int _to, Weight _cost) :from(_from), to(_to), cost(_cost) {}
-	friend ostream& operator<<(ostream& os, const Edge2& e) { return os << e.from << "->" << e.to << '(' << e.cost << ')'; }
+	friend ostream& operator<<(ostream& os, const Edge2& e) {
+		return os << e.from << "->" << e.to << '(' << e.cost << ')';
+	}
 };
 using Edges = vector<Edge2>;
 using Matrix = vector<vector<Weight>>;
