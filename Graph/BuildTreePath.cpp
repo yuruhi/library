53ba9abd9133fb3cce113145f3b83323b0b92c1c
yuruhi <yuruhiya@gmail.com>
2020-05-20 13:18:59 +0900

    fix

---
+++ Graph/BuildTreePath.cpp
@@ -0,0 +1,19 @@
+// shortcut : BuildPathTree
+// description : s-t 間のパスを求める
+vector<int> BuildPathTree(const Graph& g, int s, int t) {
+	int n = g.size();
+	vector<int> par(n);
+	function<void(int, int)> dfs = [&](int v, int p) {
+		for (const auto& u : g[v])if (u.to != p) {
+			par[u.to] = v;
+			dfs(u.to, v);
+		}
+	};
+	dfs(s, -1);
+	vector<int> path{ t };
+	while (path.back() != s) {
+		path.push_back(par[path.back()]);
+	}
+	reverse(path.begin(), path.end());
+	return path;
+}
