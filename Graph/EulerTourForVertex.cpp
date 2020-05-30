04ac7fb3e959cee92f31830c5ce8cc07cb0df251
yuruhi <yuruhiya@gmail.com>
2020-05-30 22:46:14 +0900

    add

---
+++ Graph/EulerTourForVertex.cpp
@@ -0,0 +1,34 @@
+// shortcut : EulerTourForVertex
+// description : オイラーツアー
+class EulerTourForVertex {
+	vector<int> ls, rs;
+	int pos = 0;
+	void dfs(int v, int p) {
+		ls[v] = pos++;
+		for (int u : G[v]) {
+			if (u != p)dfs(u, v);
+		}
+		rs[v] = pos;
+	}
+public:
+	vector<vector<int>> G;
+	EulerTourForVertex() {}
+	EulerTourForVertex(int n) :ls(n), rs(n), G(n) {}
+	void add_edge(int u, int v) {
+		G[u].emplace_back(v);
+		G[v].emplace_back(u);
+	}
+	void build(int r = 0) {
+		pos = 0;
+		dfs(r, -1);
+	}
+	int operator[](int v) {
+		return ls[v];
+	}
+	int r(int v) {
+		return rs[v];
+	}
+	template<typename F>void exec(int v, F f) {
+		f(ls[v], rs[v]);
+	}
+};
