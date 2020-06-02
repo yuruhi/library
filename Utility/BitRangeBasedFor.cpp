6728fac78313697373e4a791c770d805b240ebff
yuruhi <yuruhiya@gmail.com>
2020-06-02 15:43:59 +0900

    fix

--- Utility/BitRangeBasedFor.cpp
+++ Utility/BitRangeBasedFor.cpp
@@ -5,20 +5,32 @@ class bit_subset {
 		long long sub, s;
 		bool f = false;
 		it(long long x) :sub(x), s(x) {}
-		long long operator*()const { return sub; }
-		void operator++() { sub = (sub - 1) & s; f |= sub == s; }
-		bool operator!=(const it& i)const { return !f; }
+		long long operator*()const {
+			return sub;
+		}
+		void operator++() {
+			sub = (sub - 1) & s; f |= sub == s;
+		}
+		bool operator!=(const it& i)const {
+			return !f;
+		}
 	}b, e;
 public:
 	bit_subset(long long s) :b(s), e(s) {}
-	it begin()const { return b; }
-	it end()const { return e; }
+	it begin()const {
+		return b;
+	}
+	it end()const {
+		return e;
+	}
 };
 class bit_comb {
 	struct it {
 		long long s;
 		it(long long x) :s(x) {}
-		long long operator*()const { return s; }
+		long long operator*()const {
+			return s;
+		}
 		void operator++() {
 			long long x = s & -s, y = s + x;
 			s = ((s & ~y) / x >> 1) | y;
@@ -29,6 +41,10 @@ class bit_comb {
 	}b, e;
 public:
 	bit_comb(int k, int n) :b((1LL << k) - 1), e(1LL << n) {}
-	it begin()const { return b; }
-	it end()const { return e; }
+	it begin()const {
+		return b;
+	}
+	it end()const {
+		return e;
+	}
 };
