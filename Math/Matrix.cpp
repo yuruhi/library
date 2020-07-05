2bd45c7e51e75d21156fdfad3953f543e09d18e0
yuruhi <yuruhiya@gmail.com>
2020-07-05 19:18:26 +0900

    fix 'Matrix.cpp'

--- Math/Matrix.cpp
+++ Math/Matrix.cpp
@@ -4,26 +4,33 @@ template <class T> struct Matrix {
 	vector<vector<T>> A;
 
 public:
+	static Matrix I(size_t n) {
+		Matrix A(n);
+		for (size_t i = 0; i < n; ++i) {
+			A[i][i] = 1;
+		}
+		return A;
+	}
 	Matrix() {}
 	Matrix(size_t _h, size_t _w) : h(_h), w(_w), A(h, vector<T>(w, 0)) {}
 	Matrix(size_t _h) : h(_h), w(_h), A(h, vector<T>(w, 0)){};
-	Matrix(const vector<vector<T>> &_A) : h(_A.size()), w(_A[0].size()), A(_A) {}
+	Matrix(const vector<vector<T>>& _A) : h(_A.size()), w(_A[0].size()), A(_A) {}
 	size_t height() const {
 		return h;
 	}
 	size_t width() const {
 		return w;
 	}
-	const vector<T> &operator[](int i) const {
+	const vector<T>& operator[](int i) const {
 		return A[i];
 	}
-	vector<T> &operator[](int i) {
+	vector<T>& operator[](int i) {
 		return A[i];
 	}
-	const vector<vector<T>> &operator*() const {
+	const vector<vector<T>>& operator*() const {
 		return A;
 	}
-	Matrix &operator+=(const Matrix &B) {
+	Matrix& operator+=(const Matrix& B) {
 		assert(h == B.height() && w == B.width());
 		for (size_t i = 0; i < h; ++i) {
 			for (size_t j = 0; j < w; ++j) {
@@ -32,7 +39,7 @@ public:
 		}
 		return *this;
 	}
-	Matrix &operator-=(const Matrix &B) {
+	Matrix& operator-=(const Matrix& B) {
 		assert(h == B.height() && w == B.width());
 		for (size_t i = 0; i < h; ++i) {
 			for (size_t j = 0; j < w; ++j) {
@@ -41,7 +48,7 @@ public:
 		}
 		return *this;
 	}
-	Matrix &operator*=(const Matrix &B) {
+	Matrix& operator*=(const Matrix& B) {
 		size_t n = B.width();
 		assert(w == B.height());
 		vector<vector<T>> C(h, vector<T>(n, 0));
@@ -55,11 +62,8 @@ public:
 		A.swap(C);
 		return *this;
 	}
-	Matrix &operator^=(long long k) {
-		Matrix B(h);
-		for (size_t i = 0; i < h; ++i) {
-			B[i][i] = 1;
-		}
+	Matrix& operator^=(long long k) {
+		Matrix B = Matrix::I(h);
 		while (k > 0) {
 			if (k & 1) {
 				B *= *this;
@@ -70,13 +74,13 @@ public:
 		A.swap(B.A);
 		return *this;
 	}
-	Matrix operator+(const Matrix &B) const {
+	Matrix operator+(const Matrix& B) const {
 		return Matrix(*this) += B;
 	}
-	Matrix operator-(const Matrix &B) const {
+	Matrix operator-(const Matrix& B) const {
 		return Matrix(*this) -= B;
 	}
-	Matrix operator*(const Matrix &B) const {
+	Matrix operator*(const Matrix& B) const {
 		return Matrix(*this) *= B;
 	}
 	Matrix operator^(const long long k) const {
