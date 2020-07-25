9d7d785a88939664cf29ec84cfbde401bfa21003
yuruhi <yuruhiya@gmail.com>
2020-07-25 13:52:18 +0900

    fix Inversion.cpp

--- Math/Inversion.cpp
+++ Math/Inversion.cpp
@@ -1,9 +1,8 @@
 // description : 転倒数 a : { 1..N } の順列
-long long Inversion(const vector<int> a) {
-	int n = a.size();
+long long Inversion(const vector<int>& a, int max_val) {
 	long long ans = 0;
-	BinaryIndexedTree<int> bit(n);
-	for (int i = 0; i < n; ++i) {
+	BinaryIndexedTree<int> bit(max_val + 1);
+	for (size_t i = 0; i < a.size(); ++i) {
 		ans += i - bit(a[i] - 1);
 		bit.add(a[i], 1);
 	}
