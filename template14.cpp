a0b4e845147903feb6bd9b49611fb58de11132d5
yuruhi <yuruhiya@gmail.com>
2020-07-31 21:57:31 +0900

    fix template

--- template14.cpp
+++ template14.cpp
@@ -1,7 +1,7 @@
 // description : C++14用テンプレート
 #define _USE_MATH_DEFINES
 #define _CRT_SECURE_NO_WARNINGS
-#include "bits/stdc++.h"
+#include <bits/stdc++.h>
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 #define FOR(i, m, n) for (int i = (m); i < (n); ++i)
 #define rrep(i, n) for (int i = (n) - 1; i >= 0; --i)
