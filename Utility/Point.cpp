71b43463ab76031eb1652452a6e5c09e61fbebdc
yuruhi <yuruhiya@gmail.com>
2020-08-06 14:05:33 +0900

    fix Point.cpp

--- Utility/Point.cpp
+++ Utility/Point.cpp
@@ -15,7 +15,7 @@ struct Point {
 	int x, y;
 	constexpr Point() : x(0), y(0) {}
 	constexpr Point(int _x, int _y) : x(_x), y(_y) {}
-	constexpr Point(const pair<int, int> &xy) : x(xy.first), y(xy.second) {}
+	constexpr Point(const pair<int, int>& xy) : x(xy.first), y(xy.second) {}
 	Point(int n) : x(n % W), y(n / W) {}
 	constexpr Point operator+() const {
 		return *this;
@@ -23,19 +23,19 @@ struct Point {
 	constexpr Point operator-() const {
 		return {-x, -y};
 	}
-	constexpr Point operator+(const Point &p) const {
+	constexpr Point operator+(const Point& p) const {
 		return Point(*this) += p;
 	}
-	constexpr Point operator-(const Point &p) const {
+	constexpr Point operator-(const Point& p) const {
 		return Point(*this) -= p;
 	}
-	constexpr Point operator*(const Point &p) const {
+	constexpr Point operator*(const Point& p) const {
 		return Point(*this) *= p;
 	}
-	constexpr Point operator/(const Point &p) const {
+	constexpr Point operator/(const Point& p) const {
 		return Point(*this) /= p;
 	}
-	constexpr Point operator%(const Point &p) const {
+	constexpr Point operator%(const Point& p) const {
 		return Point(*this) %= p;
 	}
 	constexpr Point operator+(int n) const {
@@ -53,72 +53,72 @@ struct Point {
 	constexpr Point operator%(int n) const {
 		return Point(*this) %= n;
 	}
-	constexpr Point &operator+=(const Point &p) {
+	constexpr Point& operator+=(const Point& p) {
 		x += p.x;
 		y += p.y;
 		return *this;
 	}
-	constexpr Point &operator-=(const Point &p) {
+	constexpr Point& operator-=(const Point& p) {
 		x -= p.x;
 		y -= p.y;
 		return *this;
 	}
-	constexpr Point &operator*=(const Point &p) {
+	constexpr Point& operator*=(const Point& p) {
 		x *= p.x;
 		y *= p.y;
 		return *this;
 	}
-	constexpr Point &operator/=(const Point &p) {
+	constexpr Point& operator/=(const Point& p) {
 		x /= p.x;
 		y /= p.y;
 		return *this;
 	}
-	constexpr Point &operator%=(const Point &p) {
+	constexpr Point& operator%=(const Point& p) {
 		x %= p.x;
 		y %= p.y;
 		return *this;
 	}
-	constexpr Point &operator+=(int n) {
+	constexpr Point& operator+=(int n) {
 		x += n;
 		y += n;
 		return *this;
 	}
-	constexpr Point &operator-=(int n) {
+	constexpr Point& operator-=(int n) {
 		x -= n;
 		y -= n;
 		return *this;
 	}
-	constexpr Point &operator*=(int n) {
+	constexpr Point& operator*=(int n) {
 		x *= n;
 		y *= n;
 		return *this;
 	}
-	constexpr Point &operator/=(int n) {
+	constexpr Point& operator/=(int n) {
 		x /= n;
 		y /= n;
 		return *this;
 	}
-	constexpr Point &operator%=(int n) {
+	constexpr Point& operator%=(int n) {
 		x %= n;
 		y %= n;
 		return *this;
 	}
-	constexpr bool operator==(const Point &p) const {
+	constexpr bool operator==(const Point& p) const {
 		return x == p.x && y == p.y;
 	}
-	constexpr bool operator!=(const Point &p) const {
+	constexpr bool operator!=(const Point& p) const {
 		return x != p.x || y != p.y;
 	}
-	bool operator<(const Point &p) const {
+	bool operator<(const Point& p) const {
 		return to_i() < p.to_i();
 	}
-	bool operator<=(const Point &p) const {
+	bool operator<=(const Point& p) const {
 		return to_i() <= p.to_i();
 	}
-	bool operator>(const Point &p) const {
+	bool operator>(const Point& p) const {
 		return to_i() > p.to_i();
 	}
-	bool operator>=(const Point &p) const {
+	bool operator>=(const Point& p) const {
 		return to_i() >= p.to_i();
 	}
 	constexpr int operator[](int i) const {
@@ -133,40 +133,49 @@ struct Point {
 	constexpr pair<int, int> to_pair() const {
 		return {x, y};
 	}
-	int dist(const Point &p) const {
+	int dist(const Point& p) const {
 		return std::abs(x - p.x) + std::abs(y - p.y);
 	}
-	Point abs(const Point &p) const {
+	int dist_square(const Point& p) const {
+		return (x - p.x) * (x - p.x) + (y - p.y) * (y - p.y);
+	}
+	Point abs(const Point& p) const {
 		return {std::abs(x - p.x), std::abs(y - p.y)};
 	}
 	Point abs() const {
 		return {std::abs(x), std::abs(y)};
 	}
-	Point &swap() {
+	Point& swap() {
 		std::swap(x, y);
 		return *this;
 	}
+
+	template <class It> vector<Point> enum_adjanect(It first, It last) const {
+		vector<Point> res;
+		for (; first != last; ++first) {
+			res.push_back(operator+(*first));
+		}
+		return res;
+	}
+	template <class It> vector<Point> enum_adj_in_range(It first, It last) const {
+		vector<Point> res;
+		for (; first != last; ++first) {
+			auto p = operator+(*first);
+			if (p.in_range()) res.push_back(p);
+		}
+		return res;
+	}
 	vector<Point> adjacent4() const {
-		vector<Point> v(4);
-		for (int i = 0; i < 4; ++i) v[i] = *this + d[i];
-		return v;
+		return enum_adjanect(d.begin(), d.begin() + 4);
 	}
 	vector<Point> adjacent8() const {
-		vector<Point> v(8);
-		for (int i = 0; i < 8; ++i) v[i] = *this + d[i];
-		return v;
+		return enum_adjanect(d.begin(), d.end());
 	}
 	vector<Point> adj4_in_range() const {
-		vector<Point> v;
-		for (const auto &P : adjacent4())
-			if (P.in_range()) v.push_back(P);
-		return v;
+		return enum_adj_in_range(d.begin(), d.begin() + 4);
 	}
 	vector<Point> adj8_in_range() const {
-		vector<Point> v;
-		for (const auto &P : adjacent8())
-			if (P.in_range()) v.push_back(P);
-		return v;
+		return enum_adj_in_range(d.begin(), d.end());
 	}
 	constexpr Point left() const {
 		return {x - 1, y};
@@ -183,7 +192,7 @@ struct Point {
 	constexpr Point moved(char c) const {
 		return Point(*this).move(c);
 	}
-	constexpr Point &move(char c) {
+	constexpr Point& move(char c) {
 		switch (c) {
 			case 'L':
 			case 'l':
@@ -213,10 +222,10 @@ struct Point {
 	constexpr Point rotate270() {
 		return {-y, x};
 	}
-	friend ostream &operator<<(ostream &os, const Point &p) {
+	friend ostream& operator<<(ostream& os, const Point& p) {
 		return os << '(' << p.x << ", " << p.y << ')';
 	}
-	friend istream &operator>>(istream &is, Point &p) {
+	friend istream& operator>>(istream& is, Point& p) {
 		return is >> p.x >> p.y;
 	}
 };
