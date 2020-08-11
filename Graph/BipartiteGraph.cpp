4f7fc251265f96320f9366d697a76c1957f29897
yuruhi <yuruhiya@gmail.com>
2020-08-11 18:23:42 +0900

    fix BipartiteGraph.cpp

--- Graph/BipartiteGraph.cpp
+++ Graph/BipartiteGraph.cpp
@@ -1,19 +1,25 @@
 // description : 二部グラフ判定 O(V + E)
-int BipartiteGraph(const Graph &graph) {
-	int V = graph.size(), White = 0;
-	vector<int> Cr(V);
-	function<bool(int, int)> dfs = [&](int v, int c) {
-		Cr[v] = c;
-		White += c == 1;
-		for (auto e : graph[v]) {
-			if (Cr[e.to] == c) return false;
-			if (Cr[e.to] == 0 && !dfs(e.to, -c)) return false;
+tuple<bool, vector<bool>> BipartiteGraph(const vector<vector<int>>& g) {
+	int n = g.size();
+	vector<int> a(n);
+	bool flag = true;
+	auto dfs = [&](auto&& f, int v, int color) -> bool {
+		a[v] = color;
+		for (int u : g[v]) {
+			if (a[u] == color || (a[u] == 0 && !f(f, u, color * -1))) {
+				return false;
+			}
 		}
 		return true;
 	};
-	for (int i = 0; i < V; i++) {
-		if (Cr[i] == 0)
-			if (!dfs(i, 1)) return -1;
+	for (int i = 0; i < n; ++i) {
+		if (a[i] == 0 && !dfs(dfs, 0, 1)) {
+			return {false, vector<bool>()};
+		}
 	}
-	return White;
+	vector<bool> res(n);
+	for (int i = 0; i < n; ++i) {
+		res[i] = a[i] == 1;
+	}
+	return {flag, res};
 }
