4c7f946a4641d6aeb4b27b9a3f50118ef29c3584
yuruhi <yuruhiya@gmail.com>
2020-09-22 22:26:10 +0900

    fix RangeAddQuery.cpp

--- DataStructure/RangeAddQuery.cpp
+++ DataStructure/RangeAddQuery.cpp
@@ -1,5 +1,5 @@
 // description : 区間加算、一点取得
-template <class T> class RangaAddQuery {
+template <class T> class RangeAddQuery {
 	int n;
 	vector<T> a;
 	static int ceil2(int n) {
@@ -10,21 +10,33 @@ template <class T> class RangaAddQuery {
 	}
 
 public:
-	RangaAddQuery(int _n) : n(ceil2(_n)), a(n * 2) {}
+	RangeAddQuery(int _n) : n(ceil2(_n)), a(n * 2) {}
+	RangeAddQuery(const vector<T>& v) : n(ceil2(v.size())), a(n * 2) {
+		for (size_t i = 0; i < v.size(); ++i) {
+			a[i + n] = v[i];
+		}
+	}
 	void add(int l, int r, const T& x) {
+		l = clamp(l, 0, n);
+		r = clamp(r, 0, n);
+		if (l >= r) return;
 		for (l += n, r += n; l < r; l >>= 1, r >>= 1) {
 			if (l & 1) a[l++] += x;
 			if (r & 1) a[--r] += x;
 		}
 	}
-	T operator[](int i) {
+	void add(int i, const T& x) {
+		add(i, i + 1, x);
+	}
+	T operator[](int i) const {
+		assert(0 <= i && i < n);
 		T res = a[i += n];
 		while (i >>= 1) {
 			res += a[i];
 		}
 		return res;
 	}
-	vector<T> to_a() {
+	vector<T> to_a() const {
 		vector<T> res(n);
 		for (int i = 0; i < n; ++i) {
 			res[i] = operator[](i);
