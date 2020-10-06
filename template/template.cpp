4d038157d99721df735210d376d9f12c3df3f9b8
yuruhi <yuruhiya@gmail.com>
2020-10-06 16:34:07 +0900

    fix

--- template/template.cpp
+++ template/template.cpp
@@ -4,8 +4,8 @@
 #include "./Output.cpp"
 #include "./Step.cpp"
 #include "./Ruby.cpp"
-#if __has_include("dump.hpp")
-#include "dump.hpp"
+#if __has_include("./../dump.hpp")
+#include "./../dump.hpp"
 #else
 #define dump(...) ((void)0)
 #endif
