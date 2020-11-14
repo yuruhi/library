4d914b6c60dda040f85e461f2c28c52796417ea9
yuruhi <yuruhiya@gmail.com>
2020-11-14 19:22:16 +0900

    fix

--- Math/Inversion.cpp
+++ Math/Inversion.cpp
@@ -7,7 +7,7 @@ long long Inversion(const vector<int>& a, int max_val) {
 	long long ans = 0;
 	BinaryIndexedTree<int> bit(max_val + 1);
 	for (size_t i = 0; i < a.size(); ++i) {
-		ans += i - bit(a[i] - 1);
+		ans += i - bit(a[i]);
 		bit.add(a[i], 1);
 	}
 	return ans;
