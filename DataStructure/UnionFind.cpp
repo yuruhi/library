cecd55b16fe9b807ec06710f30fde4e4d8fc6e69
yuruhi <yuruhiya@gmail.com>
2020-11-15 14:24:17 +0900

    fix

--- DataStructure/UnionFind.cpp
+++ DataStructure/UnionFind.cpp
@@ -4,37 +4,41 @@
 using namespace std;
 
 class UnionFind {
-	vector<int> d;
-	int cnt;
+	vector<int> per_m;
+	int count_components_m;
 
 public:
 	UnionFind(int n = 0) {
 		init(n);
 	}
 	void init(int n) {
-		d.assign(n, -1);
-		cnt = n;
+		per_m.assign(n, -1);
+		count_components_m = n;
 	}
 	int root(int x) {
-		return d[x] < 0 ? x : d[x] = root(d[x]);
-	}
-	bool unite(int x, int y) {
-		x = root(x);
-		y = root(y);
-		if (x == y) return false;
-		if (d[x] > d[y]) swap(x, y);
-		d[x] += d[y];
-		d[y] = x;
-		cnt--;
-		return true;
+		return per_m[x] < 0 ? x : per_m[x] = root(per_m[x]);
 	}
 	bool same(int x, int y) {
 		return root(x) == root(y);
 	}
+	bool unite(int x, int y) {
+		x = root(x);
+		y = root(y);
+		if (x == y) {
+			return false;
+		}
+		if (per_m[x] > per_m[y]) {
+			swap(x, y);
+		}
+		per_m[x] += per_m[y];
+		per_m[y] = x;
+		count_components_m--;
+		return true;
+	}
 	int size(int x) {
-		return -d[root(x)];
+		return -per_m[root(x)];
 	}
 	int count_components() const {
-		return cnt;
+		return count_components_m;
 	}
 };
