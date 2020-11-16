c5f6e671e2bcf0edbe52a5b9dbc143ab9d5d57d6
yuruhi <yuruhiya@gmail.com>
2020-11-16 21:37:23 +0900

    Polygon::closest_pair divide other file

--- test/Geometric_closest_pair.test.cpp
+++ test/Geometric_closest_pair.test.cpp
@@ -1,18 +1,19 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/4/CGL/all/CGL_5_A"
 #define ERROR "1e-8"
-#include "./../Geometry/Polygon.hpp"
-#include "./../Geometry/Geometric.cpp"
+#include "./../Geometry/ClosestPair.cpp"
 #include <iostream>
+#include <vector>
 #include <cassert>
 using namespace std;
 
 int main() {
 	int n;
 	cin >> n;
-	Geometric::Polygon p(n);
-	for (auto& i : p)
+	vector<Geometric::Vec2> p(n);
+	for (auto& i : p) {
 		cin >> i;
-	auto [dist, p1, p2] = p.closest_pair();
+	}
+	auto [dist, p1, p2] = Geometric::closest_pair(p);
 	assert(Geometric::sgn(dist - (p1 - p2).length()) == 0);
 	printf("%.12Lf\n", dist);
 }
