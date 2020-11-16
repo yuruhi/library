e4b69ffaffa1b4f8c7e19f23ddf4bc746dbedf2b
yuruhi <yuruhiya@gmail.com>
2020-11-16 21:22:49 +0900

    fix

--- DP/LIS.cpp
+++ DP/LIS.cpp
@@ -1,13 +1,15 @@
 #pragma once
 #include <vector>
 #include <algorithm>
+#include <limits>
 using namespace std;
 
-template <class T> int LIS(const vector<T>& a, T INF_ = 1e9) {
+template <class T> int LIS(const vector<T>& a) {
+	constexpr T INF = numeric_limits<T>::max();
 	int n = a.size();
-	vector<T> dp(n, INF_);
+	vector<T> dp(n, INF);
 	for (auto num : a) {
 		*lower_bound(dp.begin(), dp.end(), num) = num;
 	}
-	return lower_bound(dp.begin(), dp.end(), INF_) - dp.begin();
+	return lower_bound(dp.begin(), dp.end(), INF) - dp.begin();
 }
