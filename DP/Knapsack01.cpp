8604635274cf4e0ebba02d303d7bb0a357989179
yuruhi <yuruhiya@gmail.com>
2020-11-16 21:19:46 +0900

    add Knapsack01

---
+++ DP/Knapsack01.cpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+template <class T> vector<T> Knapsack01(int n, int wight_limit, const vector<T>& value, const vector<int>& weight) {
+	vector<T> dp(wight_limit + 1, 0);
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j <= wight_limit - weight[i]; ++j) {
+			dp[j + weight[i]] = max(dp[j + weight[i]], dp[j] + value[i]);
+		}
+	}
+	return dp;
+}
