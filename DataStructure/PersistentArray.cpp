b8a9ec70eac49851a8f17f8eb7967b1b0036b848
yuruhi <yuruhiya@gmail.com>
2020-11-16 17:59:04 +0900

    int -> size_t

--- DataStructure/PersistentArray.cpp
+++ DataStructure/PersistentArray.cpp
@@ -4,7 +4,7 @@
 #include <memory>
 using namespace std;
 
-template <class T, int LOG> class PersistentArray {
+template <class T, size_t LOG> class PersistentArray {
 public:
 	using value_type = T;
 	using const_reference = const value_type&;
@@ -12,41 +12,42 @@ public:
 private:
 	struct node_type;
 	using node_ptr = shared_ptr<node_type>;
+	static constexpr size_t CHILD_SIZE = 1 << LOG;
 	struct node_type {
 		value_type value;
-		array<node_ptr, 1 << LOG> child;
+		array<node_ptr, CHILD_SIZE> child;
 	};
 
 	node_ptr root;
 
 	PersistentArray(node_ptr _root) : root(_root) {}
-	value_type get(int i, node_ptr p) const {
+	value_type get(size_t i, node_ptr p) const {
 		if (!p) {
 			return value_type();
 		} else if (i == 0) {
 			return p->value;
 		} else {
-			return get(i >> LOG, p->child[i & ((1 << LOG) - 1)]);
+			return get(i >> LOG, p->child[i & (CHILD_SIZE - 1)]);
 		}
 	}
-	node_ptr set(int i, const_reference val, node_ptr p) {
+	node_ptr set(size_t i, const_reference val, node_ptr p) {
 		node_ptr result = p ? make_shared<node_type>(*p) : make_shared<node_type>();
 		if (i == 0) {
 			result->value = val;
 		} else {
-			int index = i & ((1 << LOG) - 1);
+			size_t index = i & (CHILD_SIZE - 1);
 			result->child[index] = set(i >> LOG, val, result->child[index]);
 		}
 		return result;
 	}
-	void destructive_set(int i, const_reference val, node_ptr& p) {
+	void destructive_set(size_t i, const_reference val, node_ptr& p) {
 		if (!p) {
 			p = make_shared<node_type>();
 		}
 		if (i == 0) {
 			p->value = val;
 		} else {
-			int index = i & ((1 << LOG) - 1);
+			size_t index = i & (CHILD_SIZE - 1);
 			destructive_set(i >> LOG, val, p->child[index]);
 		}
 	}
@@ -58,23 +59,23 @@ public:
 			destructive_set(i, v[i]);
 		}
 	}
-	value_type get(int i) const {
+	value_type get(size_t i) const {
 		return get(i, root);
 	}
-	value_type operator[](int i) const {
+	value_type operator[](size_t i) const {
 		return get(i);
 	}
-	vector<value_type> to_a(int n) const {
+	vector<value_type> to_a(size_t n) const {
 		vector<value_type> result(n);
-		for (int i = 0; i < n; ++i) {
+		for (size_t i = 0; i < n; ++i) {
 			result[i] = get(i);
 		}
 		return result;
 	}
-	PersistentArray set(int i, const_reference val) {
+	PersistentArray set(size_t i, const_reference val) {
 		return PersistentArray(set(i, val, root));
 	}
-	void destructive_set(int i, const_reference val) {
+	void destructive_set(size_t i, const_reference val) {
 		destructive_set(i, val, root);
 	}
 };
