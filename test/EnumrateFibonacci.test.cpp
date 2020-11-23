ae5e8979a8063fb7219238659679038f5e07fc2d
yuruhi <yuruhiya@gmail.com>
2020-11-23 16:47:27 +0900

    add test

---
+++ test/EnumrateFibonacci.test.cpp
@@ -0,0 +1,10 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/all/ALDS1_10_A"
+#include "./../Math/EnumrateFibonacci.cpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	cout << EnumrateFibonacci<long long>(n + 1)[n + 1] << '\n';
+}
