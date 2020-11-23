524b0ced67ed9beda0e87856d0c327a7eb61292e
yuruhi <yuruhiya@gmail.com>
2020-11-23 16:14:10 +0900

    fix

--- Math/Sieve.cpp
+++ Math/Sieve.cpp
@@ -7,63 +7,65 @@ using namespace std;
 
 class Sieve {
 	int n;
-	vector<int> f, primes;
+	vector<int> factor, primes;
 
 public:
-	Sieve(int _n) : n(_n), f(_n + 1) {
+	Sieve(int _n) : n(_n), factor(_n + 1) {
 		assert(1 <= n);
-		f[0] = f[1] = -1;
-		for (long long i = 2; i <= n; ++i)
-			if (!f[i]) {
+		factor[0] = factor[1] = -1;
+		for (long long i = 2; i <= n; ++i) {
+			if (!factor[i]) {
 				primes.push_back(i);
-				f[i] = i;
-				for (long long j = i * i; j <= n; j += i)
-					if (!f[j]) f[j] = i;
+				factor[i] = i;
+				for (long long j = i * i; j <= n; j += i) {
+					if (!factor[j]) factor[j] = i;
+				}
 			}
+		}
 	}
 	bool is_prime(int x) const {
-		return f[x] == x;
+		return factor[x] == x;
 	}
 	const vector<int>& get_primes() const {
 		return primes;
 	}
-	vector<pair<int, int>> factor(int x) const {
+	vector<pair<int, int>> prime_factor(int x) const {
 		assert(1 <= x);
 		vector<pair<int, int>> result;
 		while (x != 1) {
-			if (result.empty() || result.back().first != f[x]) {
-				result.emplace_back(f[x], 1);
+			if (result.empty() || result.back().first != factor[x]) {
+				result.emplace_back(factor[x], 1);
 			} else {
 				result.back().second++;
 			}
-			x /= f[x];
+			x /= factor[x];
 		}
 		return result;
 	}
-	map<int, int> factor_map(int x) const {
+	map<int, int> prime_factor_map(int x) const {
 		assert(1 <= x);
 		map<int, int> result;
 		while (x != 1) {
-			result[f[x]]++;
-			x /= f[x];
+			result[factor[x]]++;
+			x /= factor[x];
 		}
 		return result;
 	}
-	vector<int> factor_vec(int x) const {
+	vector<int> prime_factor_vec(int x) const {
 		assert(1 <= x);
 		vector<int> result;
 		while (x != 1) {
-			result.push_back(f[x]);
-			x /= f[x];
+			result.push_back(factor[x]);
+			x /= factor[x];
 		}
 		return result;
 	}
-	int diviser_count(int x) const {
+	int divisors_count(int x) const {
 		assert(1 <= x);
 		int result = 1;
-		for (auto [elem, cnt] : factor(x)) {
+		for (auto [elem, cnt] : prime_factor(x)) {
 			result *= cnt + 1;
 		}
 		return result;
 	}
-}
+};
