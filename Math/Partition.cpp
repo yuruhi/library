c47e7ab16e229d94e0f16e1f23f963aee3c1deeb
yuruhi <yuruhiya@gmail.com>
2020-11-23 16:29:29 +0900

    fix and add test

--- Math/Partition.cpp
+++ Math/Partition.cpp
@@ -2,11 +2,11 @@
 #include <vector>
 using namespace std;
 
-template <class T> vector<vector<T>> Partition(int n, int m) {
-	vector<vector<T>> dp(m + 1, vector<T>(n + 1));
+template <class T> vector<vector<T>> Partition(int num, int sum) {
+	vector<vector<T>> dp(num + 1, vector<T>(sum + 1));
 	dp[0][0] = 1;
-	for (int i = 1; i <= m; ++i) {
-		for (int j = 0; j <= n; ++j) {
+	for (int i = 1; i <= num; ++i) {
+		for (int j = 0; j <= sum; ++j) {
 			if (j - i >= 0) {
 				dp[i][j] = dp[i - 1][j] + dp[i][j - i];
 			} else {
---
+++ test/Partition.test.cpp
@@ -0,0 +1,16 @@
+#define PROBLEM "https://yukicoder.me/problems/no/269"
+#include "./../Math/Partition.cpp"
+#include "./../Math/modint.cpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n, s, k;
+	cin >> n >> s >> k;
+	int sum = s - n * (n - 1) / 2 * k;
+	if (sum < 0) {
+		cout << 0 << '\n';
+	} else {
+		cout << Partition<mint>(n, sum)[n][sum] << '\n';
+	}
+}
