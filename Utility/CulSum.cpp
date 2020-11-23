6e358c59bb599d60aa043483c8edb032d69c4756
yuruhi <yuruhiya@gmail.com>
2020-11-23 10:07:19 +0900

    fix

--- Utility/CulSum.cpp
+++ Utility/CulSum.cpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <vector>
+#include <type_traits>
 #include <cassert>
 using namespace std;
 
@@ -10,35 +11,38 @@ public:
 
 private:
 	size_t n;
-	data_type s;
+	data_type data;
 
 public:
-	CulSum(const data_type& a) : n(a.size()), s(n + 1) {
+	CulSum(const data_type& a) : n(a.size()), data(n + 1) {
 		for (size_t i = 0; i < n; ++i) {
-			s[i + 1] = s[i] + a[i];
+			data[i + 1] = data[i] + a[i];
 		}
 	}
-	template <class F> CulSum(int _n, F f) : n(_n), s(n + 1) {
+	template <class U, class F, enable_if_t<is_integral<U>::value, nullptr_t> = nullptr>
+	CulSum(const U& _n, F f) : n(_n), data(n + 1) {
 		for (size_t i = 0; i < n; ++i) {
-			s[i + 1] = s[i] + static_cast<value_type>(f(i));
+			data[i + 1] = data[i] + static_cast<value_type>(f(i));
 		}
 	}
-	template <class U, class F>
+	template <class U, class F, enable_if_t<!is_integral<U>::value, nullptr_t> = nullptr>
 	CulSum(const U& a, F f)
-	    : CulSum(a.size(), [a, f](size_t i, size_t j) -> value_type { return f(a[i]); }) {
+	    : CulSum(a.size(), [a, f](size_t i) -> value_type { return f(a[i]); }) {}
+	size_t size() const {
+		return n;
 	}
 	// [l, r)
 	value_type operator()(size_t l, size_t r) const {
 		l = min(l, n);
 		r = min(r, n);
-		return l > r ? 0 : s[r] - s[l];
+		return l > r ? 0 : data[r] - data[l];
 	}
 	// [0, r)
 	value_type operator()(size_t r) const {
 		r = min(r, n);
-		return s[r];
+		return data[r];
 	}
 	const data_type& get_data() const {
-		return s;
+		return data;
 	}
 };
