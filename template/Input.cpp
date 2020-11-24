7f2bf923f56e1169c21f83e039175a906e221734
yuruhi <yuruhiya@gmail.com>
2020-11-24 20:32:53 +0900

    fix

--- template/Input.cpp
+++ template/Input.cpp
@@ -16,26 +16,24 @@ class Input {
 	static int gc() {
 		return getchar_unlocked();
 	}
-	template <class T> static void i(T& v) {
+	template <class T> static void read(T& v) {
 		cin >> v;
 	}
-	static void i(char& v) {
+	static void read(char& v) {
 		while (isspace(v = gc()))
 			;
 	}
-	static void i(bool& v) {
-		v = in<char>() != '0';
+	static void read(bool& v) {
+		v = read<char>() != '0';
 	}
-	static void i(string& v) {
+	static void read(string& v) {
 		v.clear();
-		char c;
-		for (i(c); !isspace(c); c = gc()) v += c;
+		for (char c = read<char>(); !isspace(c); c = gc()) v += c;
 	}
-	static void i(int& v) {
-		bool neg = false;
+	static void read(int& v) {
 		v = 0;
-		char c;
-		i(c);
+		bool neg = false;
+		char c = read<char>();
 		if (c == '-') {
 			neg = true;
 			c = gc();
@@ -43,11 +41,10 @@ class Input {
 		for (; isdigit(c); c = gc()) v = v * 10 + (c - '0');
 		if (neg) v = -v;
 	}
-	static void i(long long& v) {
-		bool neg = false;
+	static void read(long long& v) {
 		v = 0;
-		char c;
-		i(c);
+		bool neg = false;
+		char c = read<char>();
 		if (c == '-') {
 			neg = true;
 			c = gc();
@@ -55,74 +52,77 @@ class Input {
 		for (; isdigit(c); c = gc()) v = v * 10 + (c - '0');
 		if (neg) v = -v;
 	}
-	static void i(double& v) {
+	static void read(double& v) {
+		v = 0;
 		double dp = 1;
-		bool neg = false, adp = false;
-		v = 0;
-		char c;
-		i(c);
+		bool neg = false, after_dp = false;
+		char c = read<char>();
 		if (c == '-') {
 			neg = true;
 			c = gc();
 		}
 		for (; isdigit(c) || c == '.'; c = gc()) {
-			if (c == '.')
-				adp = true;
-			else if (adp)
+			if (c == '.') {
+				after_dp = true;
+			} else if (after_dp) {
 				v += (c - '0') * (dp *= 0.1);
-			else
+			} else {
 				v = v * 10 + (c - '0');
+			}
 		}
 		if (neg) v = -v;
 	}
-	static void i(long double& v) {
+	static void read(long double& v) {
+		v = 0;
 		long double dp = 1;
-		bool neg = false, adp = false;
-		v = 0;
-		char c;
-		i(c);
+		bool neg = false, after_dp = false;
+		char c = read<char>();
 		if (c == '-') {
 			neg = true;
 			c = gc();
 		}
 		for (; isdigit(c) || c == '.'; c = gc()) {
-			if (c == '.')
-				adp = true;
-			else if (adp)
+			if (c == '.') {
+				after_dp = true;
+			} else if (after_dp) {
 				v += (c - '0') * (dp *= 0.1);
-			else
+			} else {
 				v = v * 10 + (c - '0');
+			}
 		}
 		if (neg) v = -v;
 	}
-	template <class T, class U> static void i(pair<T, U>& v) {
-		i(v.first);
-		i(v.second);
+	template <class T, class U> static void read(pair<T, U>& v) {
+		read(v.first);
+		read(v.second);
 	}
-	template <class T> static void i(vector<T>& v) {
-		for (auto& e : v) i(e);
+	template <class T> static void read(vector<T>& v) {
+		for (auto& e : v) read(e);
 	}
-	template <size_t N = 0, class T> static void input_tuple(T& v) {
+	template <size_t N = 0, class T> static void read_tuple_impl(T& v) {
 		if constexpr (N < tuple_size_v<T>) {
-			i(get<N>(v));
-			input_tuple<N + 1>(v);
+			read(get<N>(v));
+			read_tuple_impl<N + 1>(v);
 		}
 	}
-	template <class... T> static void i(tuple<T...>& v) {
-		input_tuple(v);
+	template <class... T> static void read(tuple<T...>& v) {
+		read_tuple_impl(v);
 	}
-	struct InputV {
-		int n, m;
-		InputV(int _n) : n(_n), m(0) {}
-		InputV(const pair<int, int>& nm) : n(nm.first), m(nm.second) {}
+	struct ReadVectorHelper {
+		int n;
+		ReadVectorHelper(int _n) : n(_n) {}
 		template <class T> operator vector<T>() {
 			vector<T> v(n);
-			i(v);
+			read(v);
 			return v;
 		}
+	};
+	struct Read2DVectorHelper {
+		int n, m;
+		Read2DVectorHelper(const pair<int, int>& nm) : n(nm.first), m(nm.second) {}
 		template <class T> operator vector<vector<T>>() {
 			vector<vector<T>> v(n, vector<T>(m));
-			i(v);
+			read(v);
 			return v;
 		}
 	};
@@ -130,30 +130,29 @@ class Input {
 public:
 	static string read_line() {
 		string v;
-		char c;
-		for (i(c); c != '\n' && c != '\0'; c = gc()) v += c;
+		for (char c = read<char>(); c != '\n' && c != '\0'; c = gc()) v += c;
 		return v;
 	}
-	template <class T> static T in() {
+	template <class T> static T read() {
 		T v;
-		i(v);
+		read(v);
 		return v;
 	}
 	template <class T> operator T() const {
-		return in<T>();
+		return read<T>();
 	}
 	int operator--(int) const {
-		return in<int>() - 1;
+		return read<int>() - 1;
 	}
-	InputV operator[](int n) const {
-		return InputV(n);
+	ReadVectorHelper operator[](int n) const {
+		return ReadVectorHelper(n);
 	}
-	InputV operator[](const pair<int, int>& n) const {
-		return InputV(n);
+	Read2DVectorHelper operator[](const pair<int, int>& nm) const {
+		return Read2DVectorHelper(nm);
 	}
 	void operator()() const {}
 	template <class H, class... T> void operator()(H&& h, T&&... t) const {
-		i(h);
+		read(h);
 		operator()(forward<T>(t)...);
 	}
 
@@ -166,26 +165,22 @@ private:
 	};
 	template <template <class...> class V> struct Multiple<V> { using type = V<>; };
 	template <class... T> using multiple_t = typename Multiple<tuple, T...>::type;
-	template <size_t N = 0, class T> void in_multiple(T& t) const {
+	template <size_t N = 0, class T> void multiple_impl(T& t) const {
 		if constexpr (N < tuple_size_v<T>) {
 			auto& vec = get<N>(t);
 			using V = typename remove_reference_t<decltype(vec)>::value_type;
-			vec.push_back(in<V>());
-			in_multiple<N + 1>(t);
+			vec.push_back(read<V>());
+			multiple_impl<N + 1>(t);
 		}
 	}
 
 public:
-	template <class... T> auto multiple(int H) const {
+	template <class... T> auto multiple(int h) const {
 		multiple_t<T...> result;
-		while (H--) in_multiple(result);
+		while (h--) multiple_impl(result);
 		return result;
 	}
 } in;
-#define input(T) Input::in<T>()
-#define INT input(int)
-#define LL input(long long)
-#define STR input(string)
 #define inputs(T, ...) \
 	T __VA_ARGS__;     \
 	in(__VA_ARGS__)
