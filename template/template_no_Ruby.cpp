d4ed78e244c2cba3e89f7212e76ac98e122e2278
yuruhi <yuruhiya@gmail.com>
2020-11-26 12:19:51 +0900

    add

---
+++ template/template_no_Ruby.cpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <bits/stdc++.h>
+#include "./constants.cpp"
+#include "./Input.cpp"
+#include "./Output.cpp"
+#include "./Step.cpp"
+#include "./functions.cpp"
+#if __has_include(<library/dump.hpp>)
+#include <library/dump.hpp>
+#define LOCAL
+#else
+#define dump(...) ((void)0)
+#endif
+
+template <class T> constexpr T oj_local(const T& oj, const T& local) {
+#ifndef LOCAL
+	return oj;
+#else
+	return local;
+#endif
+}
