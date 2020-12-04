173407c3559f2afddc9c13445ba395a2d70decac
yuruhi <yuruhiya@gmail.com>
2020-12-04 20:06:01 +0900

    fix

--- Serch/GridBFS.cpp
+++ Serch/GridBFS.cpp
@@ -37,10 +37,13 @@ vector<vector<int>> GridBFS(const vector<string>& grid, char start, char wall) {
 	assert(s);
 	return GridBFS(grid, *s, wall);
 }
+int GridBFS(const vector<string>& grid, Point start, Point goal, char wall) {
+	return GridBFS(grid, start, wall)[goal.y][goal.x];
+}
 int GridBFS(const vector<string>& grid, char start, char goal, char wall) {
 	assert(start != goal);
 	Point::set_range(grid.size(), grid.front().size());
 	auto s = Point::find_one(grid, start), g = Point::find_one(grid, goal);
 	assert(s && g);
-	return GridBFS(grid, *s, wall)[g->y][g->x];
+	return GridBFS(grid, *s, *g, wall);
 }
