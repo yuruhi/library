f47682d9bd5f001a17b37df24b190134ffcda21e
yuruhi <yuruhiya@gmail.com>
2020-12-04 17:58:19 +0900

    add EnumrateEuler

--- Math/Euler.cpp
+++ Math/Euler.cpp
@@ -1,10 +1,24 @@
 #pragma once
 #include "./PrimeFactor.cpp"
+#include <numeric>
 
 template <class T> T Euler(T x) {
 	T result = x;
 	for (auto [p, e] : PrimeFactor(x)) {
 		result = result / p * (p - 1);
+	}
+	return result;
+}
+
+vector<int> EnumrateEuler(int x) {
+	vector<int> result(x + 1);
+	iota(result.begin(), result.end(), 0);
+	for (int i = 2; i <= x; ++i) {
+		if (result[i] == i) {
+			for (int j = i; j <= x; j += i) {
+				result[j] = result[j] / i * (i - 1);
+			}
+		}
 	}
 	return result;
 }
---
+++ test/EnumrateEuler.test.cpp
@@ -0,0 +1,25 @@
+#define PROBLEM \
+	"https://onlinejudge.u-aizu.ac.jp/challenges/sources/VPC/RUPC/2286?year=2011"
+#include "./../Math/Euler.cpp"
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	const int MAX_A = 1000000;
+	vector<int> euler = EnumrateEuler(MAX_A);
+	vector<long long> ans(MAX_A + 1, 1);
+	for (int i = 1; i <= MAX_A; ++i) {
+		ans[i] = ans[i - 1] + euler[i];
+	}
+
+	int q;
+	cin >> q;
+	while (q--) {
+		int x;
+		cin >> x;
+		cout << ans[x] << '\n';
+	}
+}
