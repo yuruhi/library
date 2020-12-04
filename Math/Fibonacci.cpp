258393e5d4131ac5015c55d7b3d26de6fa9ae8bb
yuruhi <yuruhiya@gmail.com>
2020-12-04 17:58:12 +0900

    fibonacci

--- Math/Fibonacci.cpp
+++ Math/Fibonacci.cpp
@@ -8,3 +8,15 @@ template <class value_type> value_type Fibonacci(long long n) {
 	Matrix<value_type> B(vector<vector<value_type>>{{1}, {0}});
 	return (A.pow(n) * B)[1][0];
 }
+
+template <class value_type = long long> vector<value_type> EnumrateFibonacci(int n) {
+	vector<value_type> result(n + 1);
+	for (int i = 0; i <= n; ++i) {
+		if (i < 2) {
+			result[i] = i;
+		} else {
+			result[i] = result[i - 1] + result[i - 2];
+		}
+	}
+	return result;
+}
