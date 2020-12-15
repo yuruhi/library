e338ef438cf28a1accb5765ec8a983c148d20071
yuruhi <yuruhiya@gmail.com>
2020-12-15 12:46:07 +0900

    rm DijkstraST.cpp

--- Graph/Dijkstra.cpp
+++ Graph/Dijkstra.cpp
@@ -24,3 +24,24 @@ vector<Weight> Dijkstra(const Graph& graph, int s) {
 	}
 	return dist;
 }
+Weight Dijkstra(const Graph& graph, int s, int t) {
+	int V = graph.size();
+	vector<Weight> dist(V, INF);
+	dist[s] = 0;
+	priority_queue<Edge, vector<Edge>, greater<Edge>> pq;
+	pq.emplace(s, 0);
+	while (!pq.empty()) {
+		Edge p = pq.top();
+		pq.pop();
+		int v = p.to;
+		if (v == t) return dist[t];
+		if (dist[v] < p.cost) continue;
+		for (auto e : graph[v]) {
+			if (dist[e.to] > dist[v] + e.cost) {
+				dist[e.to] = dist[v] + e.cost;
+				pq.emplace(e.to, dist[e.to]);
+			}
+		}
+	}
+	return dist[t];
+}
