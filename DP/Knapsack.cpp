431ad8d0192c7677e566b0ece931ff3f8368639b
yuruhi <yuruhiya@gmail.com>
2020-12-20 17:36:03 +0900

    add assert

--- DP/Knapsack.cpp
+++ DP/Knapsack.cpp
@@ -6,6 +6,8 @@ using namespace std;
 template <class T>
 vector<T> Knapsack(int n, int wight_limit, const vector<T>& value,
                    const vector<int>& weight) {
+	assert(n == static_cast<int>(value.size()));
+	assert(n == static_cast<int>(weight.size()));
 	vector<T> dp(wight_limit + 1, 0);
 	for (int i = 0; i < n; ++i) {
 		for (int j = 0; j <= wight_limit - weight[i]; ++j) {
--- DP/Knapsack01.cpp
+++ DP/Knapsack01.cpp
@@ -1,10 +1,13 @@
 #pragma once
 #include <vector>
 #include <algorithm>
+#include <cassert>
 using namespace std;
 
 template <class T>
 vector<T> Knapsack01(int n, int weight_limit, const vector<T>& value, const vector<int>& weight) {
+	assert(n == static_cast<int>(value.size()));
+	assert(n == static_cast<int>(weight.size()));
 	vector<T> dp(weight_limit + 1, 0);
 	for (int i = 0; i < n; ++i) {
 		for (int j = weight_limit; j >= 0; --j) {
