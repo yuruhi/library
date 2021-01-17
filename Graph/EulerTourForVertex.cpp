dbe7c32dfbe88c8b300976e44897d8ddf6e0f3ab
yuruhi <yuruhiya@gmail.com>
2021-01-17 16:23:07 +0900

    fix

--- Graph/EulerTourForVertex.cpp
+++ Graph/EulerTourForVertex.cpp
@@ -5,13 +5,13 @@ using namespace std;
 class EulerTourForVertex {
 	vector<int> ls, rs;
 	vector<vector<int>> graph;
-	int pos = 0;
+	int k = 0;
 	void dfs(int v, int p) {
-		ls[v] = pos++;
+		ls[v] = k++;
 		for (int u : graph[v]) {
 			if (u != p) dfs(u, v);
 		}
-		rs[v] = pos;
+		rs[v] = k;
 	}
 
 public:
@@ -21,17 +21,26 @@ public:
 		graph[u].emplace_back(v);
 		graph[v].emplace_back(u);
 	}
-	void build(int r = 0) {
-		pos = 0;
-		dfs(r, -1);
+	void build(int root) {
+		k = 0;
+		dfs(root, -1);
 	}
-	int operator[](int v) {
+	int get_l(int v) const {
 		return ls[v];
 	}
-	int r(int v) {
+	int get_r(int v) const {
 		return rs[v];
 	}
-	template <typename F> void exec(int v, F f) {
+	pair<int, int> get(int v) const {
+		return {ls[v], rs[v]};
+	}
+	const vector<int>& get_ls() const {
+		return ls;
+	}
+	const vector<int>& get_rs() const {
+		return rs;
+	}
+	template <typename F> void call(int v, F f) {
 		f(ls[v], rs[v]);
 	}
 };
