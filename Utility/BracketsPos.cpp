f1230fd8fa52eb35a15f169bb585db7200f1c709
yuruhi <yuruhiya@gmail.com>
2021-02-16 20:22:20 +0900

    change variable name

--- Utility/BracketsPos.cpp
+++ Utility/BracketsPos.cpp
@@ -6,13 +6,13 @@
 #include <cassert>
 using namespace std;
 
-size_t BracketsPos(const string& s, size_t pos, char c1 = '(', char c2 = ')') {
-	assert(s[pos] == c1);
+size_t BracketsPos(const string& s, size_t pos, char left = '(', char right = ')') {
+	assert(s[pos] == left);
 	int p = 1;
 	for (size_t i = pos + 1; i < s.size(); ++i) {
-		if (s[i] == c1) {
+		if (s[i] == left) {
 			p++;
-		} else if (s[i] == c2) {
+		} else if (s[i] == right) {
 			if (p == 1) {
 				return i;
 			}
@@ -22,10 +22,11 @@ size_t BracketsPos(const string& s, size_t pos, char c1 = '(', char c2 = ')') {
 	return string::npos;
 }
 
-pair<size_t, size_t> BracketsPos2(const string& s, size_t pos, char c1 = '(', char c2 = ')') {
-	size_t l = s.find(c1, pos);
+pair<size_t, size_t> BracketsPosPair(const string& s, size_t pos, char left = '(',
+                                     char right = ')') {
+	size_t l = s.find(left, pos);
 	if (l != string::npos) {
-		size_t r = BracketsPos(s, l, c1, c2);
+		size_t r = BracketsPos(s, l, left, right);
 		if (r != string::npos) {
 			return {l, r};
 		} else {
@@ -36,14 +37,14 @@ pair<size_t, size_t> BracketsPos2(const string& s, size_t pos, char c1 = '(', ch
 	}
 }
 
-vector<int> AllBracketsPos(const string& s, char c1 = '(', char c2 = ')') {
+vector<int> AllBracketsPos(const string& s, char left = '(', char right = ')') {
 	int n = s.size();
 	vector<int> result(n, -1);
 	stack<int> st;
 	for (int i = 0; i < n; ++i) {
-		if (s[i] == c1) {
+		if (s[i] == left) {
 			st.push(i);
-		} else if (s[i] == c2) {
+		} else if (s[i] == right) {
 			assert(st.size());
 			result[st.top()] = i;
 			st.pop();
