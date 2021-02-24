5b1f10cc15b8b18d99628aa2e2f63a40525a7bbe
yuruhi <yuruhiya@gmail.com>
2021-02-24 20:48:56 +0900

    add UnionFind::roots, UnionFind::groups

--- DataStructure/UnionFind.cpp
+++ DataStructure/UnionFind.cpp
@@ -1,17 +1,18 @@
 #pragma once
 #include <vector>
 #include <utility>
-using namespace std;
 
 class UnionFind {
-	vector<int> data_m;
+	std::size_t n;
+	std::vector<int> data_m;
 	int count_components_m;
 
 public:
-	UnionFind(int n = 0) {
-		init(n);
+	UnionFind(int _n = 0) {
+		init(_n);
 	}
-	void init(int n) {
+	void init(int _n) {
+		n = _n;
 		data_m.assign(n, -1);
 		count_components_m = n;
 	}
@@ -28,7 +29,7 @@ public:
 			return false;
 		}
 		if (data_m[x] > data_m[y]) {
-			swap(x, y);
+			std::swap(x, y);
 		}
 		data_m[x] += data_m[y];
 		data_m[y] = x;
@@ -40,5 +41,30 @@ public:
 	}
 	int count_components() const {
 		return count_components_m;
+	}
+	std::vector<int> roots() {
+		std::vector<int> result;
+		for (std::size_t i = 0; i < n; ++i) {
+			if (root(i) == i) result.push_back(i);
+		}
+		return result;
+	}
+	std::vector<std::vector<int>> groups() {
+		std::vector<int> root_buf(n), group_size(n);
+		for (std::size_t i = 0; i < n; i++) {
+			root_buf[i] = root(i);
+			group_size[root_buf[i]]++;
+		}
+		std::vector<std::vector<int>> result(n);
+		for (std::size_t i = 0; i < n; i++) {
+			result[i].reserve(group_size[i]);
+		}
+		for (std::size_t i = 0; i < n; i++) {
+			result[root_buf[i]].push_back(i);
+		}
+		result.erase(std::remove_if(result.begin(), result.end(),
+		                            [&](const std::vector<int>& v) { return v.empty(); }),
+		             result.end());
+		return result;
 	}
 };
