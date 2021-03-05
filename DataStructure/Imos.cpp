5af51faa9c9a6842ce786252c19cb505c26541e1
yuruhi <yuruhiya@gmail.com>
2021-03-05 15:06:03 +0900

    remove using namespace std;

--- DataStructure/Imos.cpp
+++ DataStructure/Imos.cpp
@@ -3,41 +3,40 @@
 #include <utility>
 #include <algorithm>
 #include <cassert>
-using namespace std;
 
 template <class T> class Imos {
 public:
 	using value_type = T;
 
 private:
-	int N;
-	vector<value_type> table;
+	int n;
+	std::vector<value_type> table;
 	bool builded = false;
 
 public:
 	Imos() = default;
-	Imos(int _n) : N(_n), table(_n + 1) {}
+	Imos(int _n) : n(_n), table(_n + 1) {}
 	void add(int l, int r, value_type v = 1) {
 		assert(!builded);
-		l = clamp(l, 0, N);
-		r = clamp(r, 0, N);
+		l = std::clamp(l, 0, n);
+		r = std::clamp(r, 0, n);
 		if (l < r) {
 			table[l] += v;
 			table[r] -= v;
 		}
 	}
-	void add(const pair<int, int>& section, value_type v = 1) {
+	void add(const std::pair<int, int>& section, value_type v = 1) {
 		add(section.first, section.second, v);
 	}
 	void add_closed(int l, int r, value_type v = 1) {
 		add(l, r + 1, v);
 	}
-	void add_closed(const pair<int, int>& section, value_type v = 1) {
+	void add_closed(const std::pair<int, int>& section, value_type v = 1) {
 		add_closed(section.first, section.second, v);
 	}
 	void build() {
 		builded = true;
-		for (int i = 1; i < N; ++i) {
+		for (int i = 1; i < n; ++i) {
 			table[i] += table[i - 1];
 		}
 	}
@@ -45,11 +44,11 @@ public:
 		assert(builded);
 		return table[i];
 	}
-	const vector<value_type>& operator*() const {
+	const std::vector<value_type>& operator*() const {
 		assert(builded);
 		return table;
 	}
-	vector<value_type> to_a() const {
-		return vector(table.begin(), table.begin() + N);
+	std::vector<value_type> to_a() const {
+		return std::vector(table.begin(), table.begin() + n);
 	}
 };
