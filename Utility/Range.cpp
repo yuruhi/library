a16cda4a98ba4a4897fa502bab79b0d594111bce
yuruhi <yuruhiya@gmail.com>
2021-03-06 16:44:53 +0900

    remove using namespace std; to_pair() -> pair() add anti_pair(), cmp_{x, y, xy, yx}

--- Utility/Range.cpp
+++ Utility/Range.cpp
@@ -4,11 +4,25 @@
 #include <iostream>
 #include <cassert>
 #include <optional>
-using namespace std;
 
 template <class T> struct Range {
 	using value_type = T;
+
 	value_type x, y;  // [x, y]
+
+	static constexpr bool cmp_x(const Range& r1, const Range& r2) {
+		return r1.x < r2.x;
+	}
+	static constexpr bool cmp_y(const Range& r1, const Range& r2) {
+		return r1.y < r2.y;
+	}
+	static constexpr bool cmp_xy(const Range& r1, const Range& r2) {
+		return r1.pair() < r2.pair();
+	}
+	static constexpr bool cmp_yx(const Range& r1, const Range& r2) {
+		return r1.anti_pair() < r2.anti_pair();
+	}
+
 	constexpr Range() : x(0), y(0) {}
 	constexpr Range(value_type _x, value_type _y, bool exclude_end = false)
 	    : x(_x), y(_y - !exclude_end) {
@@ -25,16 +39,16 @@ template <class T> struct Range {
 		return x != r.x || y != r.y;
 	}
 	constexpr bool operator<(const Range& r) const {
-		return to_pair() < r.to_pair();
+		return pair() < r.pair();
 	}
 	constexpr bool operator<=(const Range& r) const {
-		return to_pair() <= r.to_pair();
+		return pair() <= r.pair();
 	}
 	constexpr bool operator>(const Range& r) const {
-		return to_pair() > r.to_pair();
+		return pair() > r.pair();
 	}
 	constexpr bool operator>=(const Range& r) const {
-		return to_pair() >= r.to_pair();
+		return pair() >= r.pair();
 	}
 	constexpr Range& operator<<=(const value_type& n) {
 		x -= n;
@@ -60,18 +74,18 @@ template <class T> struct Range {
 	constexpr Range operator*(const value_type& n) const {
 		return Range(*this) *= n;
 	}
-	constexpr optional<Range> operator&(const Range& r) const {
+	constexpr std::optional<Range> operator&(const Range& r) const {
 		if (intersects(r)) {
 			return Range(std::max(x, r.x), std::min(y, r.y));
 		} else {
-			return nullopt;
+			return std::nullopt;
 		}
 	}
-	constexpr optional<Range> operator|(const Range& r) const {
+	constexpr std::optional<Range> operator|(const Range& r) const {
 		if (intersects(r) || adjacent(r)) {
 			return Range(std::min(x, r.x), std::max(y, r.y));
 		} else {
-			return nullopt;
+			return std::nullopt;
 		}
 	}
 	constexpr value_type operator[](const value_type& n) const {  // 0-indexed
@@ -81,8 +95,11 @@ template <class T> struct Range {
 	constexpr Range operator[](const Range& n) const {
 		return Range(operator[](n.x), operator[](n.y));
 	}
-	constexpr pair<value_type, value_type> to_pair() const {
-		return make_pair(x, y);
+	constexpr auto pair() const {
+		return std::pair(x, y);
+	}
+	constexpr auto anti_pair() const {
+		return std::pair(y, x);
 	}
 	constexpr Range open() const {
 		return Range(x, y + 1);
@@ -100,21 +117,21 @@ template <class T> struct Range {
 		return y + 1 == r.x || r.y + 1 == x;
 	}
 	constexpr value_type size() const {
-		return max<value_type>(0, y - x + 1);
+		return std::max<value_type>(0, y - x + 1);
 	}
 	constexpr value_type sum() const {
 		return (x + y) * size() / 2;
 	}
-	friend ostream& operator<<(ostream& os, const Range& r) {
+	friend std::ostream& operator<<(std::ostream& os, const Range& r) {
 		return os << '[' << r.x << ", " << r.y << ']';
 	}
-	friend istream& operator>>(istream& is, Range& r) {
+	friend std::istream& operator>>(std::istream& is, Range& r) {
 		return is >> r.x >> r.y;
 	}
 };
 template <class T, class U>
-constexpr optional<Range<T>> equal_range(const U& a, const Range<T>& r) {
-	auto L = lower_bound(a.begin(), a.end(), r.x) - a.begin();
-	auto R = upper_bound(a.begin(), a.end(), r.y) - a.begin();
-	return L == R ? nullopt : Range<T>(L, R, true);
+constexpr std::optional<Range<T>> equal_range(const U& a, const Range<T>& r) {
+	auto L = std::lower_bound(a.begin(), a.end(), r.x) - a.begin();
+	auto R = std::upper_bound(a.begin(), a.end(), r.y) - a.begin();
+	return L == R ? std::nullopt : Range<T>(L, R, true);
 }
