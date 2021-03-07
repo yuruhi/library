0a16d99ef3773445a8ab36cc328d5fe99e59df15
yuruhi <yuruhiya@gmail.com>
2021-03-07 14:48:17 +0900

    remove using namespace std;

--- Utility/RunLengthEncoding.cpp
+++ Utility/RunLengthEncoding.cpp
@@ -2,11 +2,10 @@
 #include <vector>
 #include <iterator>
 #include <utility>
-using namespace std;
 
 template <class InputIterator, class value_type = typename InputIterator::value_type>
 auto RunLengthEncoding(InputIterator first, InputIterator last) {
-	vector<pair<size_t, value_type>> result;
+	std::vector<std::pair<std::size_t, value_type>> result;
 	for (; first != last; ++first) {
 		if (result.empty() || result.back().second != *first) {
 			result.emplace_back(1, *first);
@@ -17,15 +16,14 @@ auto RunLengthEncoding(InputIterator first, InputIterator last) {
 	return result;
 }
 
-template <class Container, class value_type = typename Container::value_type>
-auto RunLengthEncoding(const Container& a) {
+template <class Container> auto RunLengthEncoding(const Container& a) {
 	return RunLengthEncoding(a.begin(), a.end());
 }
 
 template <class InputIterator, class value_type = typename InputIterator::value_type>
 auto RunLengthEncoding_pair(InputIterator first, InputIterator last) {
-	vector<size_t> count;
-	vector<value_type> value;
+	std::vector<std::size_t> count;
+	std::vector<value_type> value;
 	for (; first != last; ++first) {
 		if (value.empty() || value.back() != *first) {
 			count.push_back(1);
@@ -34,10 +32,9 @@ auto RunLengthEncoding_pair(InputIterator first, InputIterator last) {
 			count.back()++;
 		}
 	}
-	return pair(count, value);
+	return std::pair(count, value);
 }
 
-template <class Container, class value_type = typename Container::value_type>
-auto RunLengthEncoding_pair(const Container& a) {
+template <class Container> auto RunLengthEncoding_pair(const Container& a) {
 	return RunLengthEncoding_pair(a.begin(), a.end());
 }
