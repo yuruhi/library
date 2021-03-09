877fbab880e7633316dfadec3bfbd124eb60e8de
yuruhi <yuruhiya@gmail.com>
2021-03-09 16:10:12 +0900

    remove using namespace std;

--- DataStructure/CulSum.cpp
+++ DataStructure/CulSum.cpp
@@ -1,49 +1,51 @@
 #pragma once
 #include <vector>
+#include <algorithm>
 #include <type_traits>
-#include <cassert>
-using namespace std;
 
 template <class T> class CulSum {
 public:
 	using value_type = T;
-	using data_type = vector<value_type>;
+	using data_type = std::vector<value_type>;
+	using size_type = std::size_t;
 
 private:
-	size_t n;
+	size_type n;
 	data_type data;
 
 public:
 	CulSum() = default;
 	CulSum(const data_type& a) : n(a.size()), data(n + 1) {
-		for (size_t i = 0; i < n; ++i) {
+		for (size_type i = 0; i < n; ++i) {
 			data[i + 1] = data[i] + a[i];
 		}
 	}
-	template <class U, class F, enable_if_t<is_integral_v<U>, nullptr_t> = nullptr>
+	template <class U, class F,
+	          std::enable_if_t<std::is_integral_v<U>, std::nullptr_t> = nullptr>
 	CulSum(const U& _n, F f) : n(_n), data(n + 1) {
-		for (size_t i = 0; i < n; ++i) {
+		for (size_type i = 0; i < n; ++i) {
 			data[i + 1] = data[i] + static_cast<value_type>(f(i));
 		}
 	}
-	template <class U, class F, enable_if_t<!is_integral_v<U>, nullptr_t> = nullptr>
+	template <class U, class F,
+	          std::enable_if_t<!std::is_integral_v<U>, std::nullptr_t> = nullptr>
 	CulSum(const U& a, F f)
-	    : CulSum(a.size(), [a, f](size_t i) -> value_type { return f(a[i]); }) {}
-	size_t size() const {
+	    : CulSum(a.size(), [a, f](size_type i) -> value_type { return f(a[i]); }) {}
+	size_type size() const {
 		return n;
 	}
-	value_type at(size_t i) const {
+	value_type at(size_type i) const {
 		return operator()(i, i + 1);
 	}
 	// [l, r)
-	value_type operator()(size_t l, size_t r) const {
-		l = min(l, n);
-		r = min(r, n);
+	value_type operator()(size_type l, size_type r) const {
+		l = std::min(l, n);
+		r = std::min(r, n);
 		return l > r ? 0 : data[r] - data[l];
 	}
 	// [0, r)
-	value_type operator()(size_t r) const {
-		r = min(r, n);
+	value_type operator()(size_type r) const {
+		r = std::min(r, n);
 		return data[r];
 	}
 	const data_type& get_data() const {
--- DataStructure/CulSum2D.cpp
+++ DataStructure/CulSum2D.cpp
@@ -1,54 +1,57 @@
 #pragma once
 #include <vector>
 #include <algorithm>
-using namespace std;
+#include <type_traits>
 
 template <class T> class CulSum2D {
 public:
 	using value_type = T;
-	using data_type = vector<vector<value_type>>;
+	using data_type = std::vector<std::vector<value_type>>;
+	using size_type = std::size_t;
 
 private:
-	size_t h, w;
-	vector<vector<T>> data;
+	size_type h, w;
+	data_type data;
 
 public:
 	CulSum2D() = default;
 	CulSum2D(const data_type& a)
-	    : h(a.size()), w(a.front().size()), data(h + 1, vector<value_type>(w + 1)) {
-		for (size_t i = 0; i < h; ++i) {
-			for (size_t j = 0; j < w; ++j) {
+	    : h(a.size()), w(a.front().size()), data(h + 1, std::vector<value_type>(w + 1)) {
+		for (size_type i = 0; i < h; ++i) {
+			for (size_type j = 0; j < w; ++j) {
 				data[i + 1][j + 1] = data[i][j + 1] + data[i + 1][j] - data[i][j] + a[i][j];
 			}
 		}
 	}
-	template <class U, class F, enable_if_t<is_integral_v<U>, nullptr_t> = nullptr>
+	template <class U, class F,
+	          std::enable_if_t<std::is_integral_v<U>, std::nullptr_t> = nullptr>
 	CulSum2D(const U& _h, const U& _w, F f)
-	    : h(_h), w(_w), data(h + 1, vector<value_type>(w + 1)) {
-		for (size_t i = 0; i < h; ++i) {
-			for (size_t j = 0; j < w; ++j) {
+	    : h(_h), w(_w), data(h + 1, std::vector<value_type>(w + 1)) {
+		for (size_type i = 0; i < h; ++i) {
+			for (size_type j = 0; j < w; ++j) {
 				data[i + 1][j + 1] = data[i][j + 1] + data[i + 1][j] - data[i][j] + f(i, j);
 			}
 		}
 	}
-	template <class U, class F, enable_if_t<!is_integral_v<U>, nullptr_t> = nullptr>
+	template <class U, class F,
+	          std::enable_if_t<!std::is_integral_v<U>, std::nullptr_t> = nullptr>
 	CulSum2D(const U& a, F f)
 	    : CulSum2D(a.size(), a.front().size(),
-	               [a, f](size_t i, size_t j) -> value_type { return f(a[i][j]); }) {}
+	               [a, f](size_type i, size_type j) -> value_type { return f(a[i][j]); }) {}
 	// [y1, y2) * [x1, x2)
-	value_type operator()(size_t y1, size_t y2, size_t x1, size_t x2) const {
-		y1 = min(y1, h);
-		y2 = min(y2, h);
-		x1 = min(x1, w);
-		x2 = min(x2, w);
+	value_type operator()(size_type y1, size_type y2, size_type x1, size_type x2) const {
+		y1 = std::min(y1, h);
+		y2 = std::min(y2, h);
+		x1 = std::min(x1, w);
+		x2 = std::min(x2, w);
 		return (x1 > x2 || y1 > y2)
 		    ? 0
 		    : data[y2][x2] - data[y1][x2] - data[y2][x1] + data[y1][x1];
 	}
 	// [0, y) * [0, x)
-	value_type operator()(size_t y, size_t x) const {
-		y = min(y, h);
-		x = min(x, w);
+	value_type operator()(size_type y, size_type x) const {
+		y = std::min(y, h);
+		x = std::min(x, w);
 		return data[y][x];
 	}
 	const data_type& get_data() const {
