671b8d9330f87aabe801a8d480bcc24f684acfe2
yuruhi <yuruhiya@gmail.com>
2021-03-10 14:57:29 +0900

    remove using namespace std;

--- Utility/Random.cpp
+++ Utility/Random.cpp
@@ -3,11 +3,10 @@
 #include <algorithm>
 #include <type_traits>
 #include <cassert>
-using namespace std;
 
 class Random {
-	mt19937 engine;
-	random_device rd;
+	std::mt19937 engine;
+	std::random_device rd;
 
 public:
 	Random() {
@@ -16,36 +15,38 @@ public:
 	void seed() {
 		engine.seed(rd());
 	}
-	void seed(uint_fast32_t s) {
+	void seed(std::uint_fast32_t s) {
 		engine.seed(s);
 	}
 	template <class T,
-	          enable_if_t<is_integral_v<T> && !is_same_v<bool, T>, nullptr_t> = nullptr>
+	          std::enable_if_t<std::is_integral_v<T> && !std::is_same_v<bool, T>,
+	                           std::nullptr_t> = nullptr>
 	T get(T l, T r) {
 		assert(l <= r);
-		return uniform_int_distribution<T>(l, r)(engine);
+		return std::uniform_int_distribution<T>(l, r)(engine);
 	}
-	template <class T, enable_if_t<is_floating_point_v<T>, nullptr_t> = nullptr>
+	template <class T, std::enable_if_t<std::is_floating_point_v<T>, std::nullptr_t> = nullptr>
 	T get(T l, T r) {
 		assert(l <= r);
 		return uniform_real_distribution<T>(l, r)(engine);
 	}
-	template <class T, enable_if_t<is_same_v<bool, T>, nullptr_t> = nullptr>
+	template <class T, std::enable_if_t<std::is_same_v<bool, T>, std::nullptr_t> = nullptr>
 	T get(double probability = 0.5) {
 		bernoulli_distribution u(probability);
 		return u(engine);
 	}
-	template <class T, enable_if_t<is_same_v<string, T>, nullptr_t> = nullptr>
+	template <class T,
+	          std::enable_if_t<std::is_same_v<std::string, T>, std::nullptr_t> = nullptr>
 	T get(size_t n, string_view chars = "abcdefghjiklmnopqrstuvwxyz") {
 		string result;
 		result.reserve(n);
-		sample(chars.begin(), chars.end(), back_inserter(result), n, engine);
+		std::sample(chars.begin(), chars.end(), std::back_inserter(result), n, engine);
 		return result;
 	}
 	int dice() {
 		return get<int>(1, 7);
 	}
 	template <class T> void shuffle(T& v) {
-		std::shuffle(begin(v), end(v), engine);
+		std::shuffle(std::begin(v), std::end(v), engine);
 	}
 } rnd;
