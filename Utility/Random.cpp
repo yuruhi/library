799c7f042e50b6243a8380e21b90d1c6a59c0ddb
yuruhi <yuruhiya@gmail.com>
2021-03-10 22:57:13 +0900

    fix CE

--- Utility/Random.cpp
+++ Utility/Random.cpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <random>
 #include <algorithm>
+#include <string>
+#include <string_view>
 #include <type_traits>
 #include <cassert>
 
@@ -28,23 +30,26 @@ public:
 	template <class T, std::enable_if_t<std::is_floating_point_v<T>, std::nullptr_t> = nullptr>
 	T get(T l, T r) {
 		assert(l <= r);
-		return uniform_real_distribution<T>(l, r)(engine);
+		return std::uniform_real_distribution<T>(l, r)(engine);
 	}
 	template <class T, std::enable_if_t<std::is_same_v<bool, T>, std::nullptr_t> = nullptr>
 	T get(double probability = 0.5) {
-		bernoulli_distribution u(probability);
+		std::bernoulli_distribution u(probability);
 		return u(engine);
 	}
 	template <class T,
 	          std::enable_if_t<std::is_same_v<std::string, T>, std::nullptr_t> = nullptr>
-	T get(size_t n, string_view chars = "abcdefghjiklmnopqrstuvwxyz") {
-		string result;
+	T get(size_t n, std::string_view chars = "abcdefghjiklmnopqrstuvwxyz") {
+		std::string result;
 		result.reserve(n);
 		std::sample(chars.begin(), chars.end(), std::back_inserter(result), n, engine);
 		return result;
 	}
 	int dice() {
 		return get<int>(1, 7);
+	}
+	template <class T> auto sample(const T& v) {
+		return v[get<std::size_t>(0, std::size(v) - 1)];
 	}
 	template <class T> void shuffle(T& v) {
 		std::shuffle(std::begin(v), std::end(v), engine);
