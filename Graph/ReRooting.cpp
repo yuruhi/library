c1893765f8ca05ed73c9ca40ad5cb4172db8e1ff
yuruhi <yuruhiya@gmail.com>
2021-03-17 10:25:25 +0900

    remove using namespace std;

--- Graph/ReRooting.cpp
+++ Graph/ReRooting.cpp
@@ -1,16 +1,15 @@
 #pragma once
 #include <vector>
-using namespace std;
 
 template <class DP> class ReRooting {
-	size_t n;
-	vector<vector<int>> graph;
-	vector<vector<DP>> dp;
-	vector<DP> ans;
+	std::size_t n;
+	std::vector<std::vector<int>> graph;
+	std::vector<std::vector<DP>> dp;
+	std::vector<DP> ans;
 
 	DP dfs(int v, int p) {
 		DP sum;
-		for (size_t i = 0; i < graph[v].size(); ++i) {
+		for (std::size_t i = 0; i < graph[v].size(); ++i) {
 			int e = graph[v][i];
 			DP& dp_e = dp[v][i];
 			if (e != p) {
@@ -21,23 +20,23 @@ template <class DP> class ReRooting {
 		return sum.add_root(v);
 	}
 	void bfs(int v, int p, const DP& dp_par) {
-		for (size_t i = 0; i < graph[v].size(); ++i) {
+		for (std::size_t i = 0; i < graph[v].size(); ++i) {
 			if (graph[v][i] == p) {
 				dp[v][i] = dp_par;
 			}
 		}
 
-		vector<DP> dp_left(graph[v].size() + 1);
-		for (size_t i = 0; i < graph[v].size(); ++i) {
+		std::vector<DP> dp_left(graph[v].size() + 1);
+		for (std::size_t i = 0; i < graph[v].size(); ++i) {
 			dp_left[i + 1] = dp_left[i] + dp[v][i];
 		}
-		vector<DP> dp_right(graph[v].size() + 1);
+		std::vector<DP> dp_right(graph[v].size() + 1);
 		for (int i = graph[v].size() - 1; i >= 0; --i) {
 			dp_right[i] = dp_right[i + 1] + dp[v][i];
 		}
 		ans[v] = dp_left.back().add_root(v);
 
-		for (size_t i = 0; i < graph[v].size(); ++i) {
+		for (std::size_t i = 0; i < graph[v].size(); ++i) {
 			int e = graph[v][i];
 			if (e != p) {
 				bfs(e, v, (dp_left[i] + dp_right[i + 1]).add_root(v));
@@ -46,15 +45,15 @@ template <class DP> class ReRooting {
 	}
 
 public:
-	ReRooting(size_t _n) : n(_n), graph(n), dp(n), ans(n) {}
-	ReRooting(const vector<vector<int>>& _graph)
+	ReRooting(std::size_t _n) : n(_n), graph(n), dp(n), ans(n) {}
+	ReRooting(const std::vector<std::vector<int>>& _graph)
 	    : n(_graph.size()), graph(_graph), dp(n), ans(n) {}
 	void add_edge(int v, int u) {
 		graph[v].push_back(u);
 		graph[u].push_back(v);
 	}
-	vector<DP> solve() {
-		for (size_t i = 0; i < n; ++i) {
+	std::vector<DP> solve() {
+		for (std::size_t i = 0; i < n; ++i) {
 			dp[i].assign(graph[i].size(), DP());
 		}
 		dfs(0, -1);
