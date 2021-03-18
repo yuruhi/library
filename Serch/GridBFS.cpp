747d98e2b9f7f2add6befcee1cfacb9ae927b573
yuruhi <yuruhiya@gmail.com>
2021-03-18 14:11:22 +0900

    remove using namespace std;

--- Serch/GridBFS.cpp
+++ Serch/GridBFS.cpp
@@ -6,24 +6,24 @@
 #include <optional>
 #include <limits>
 #include <cassert>
-using namespace std;
 
-vector<vector<int>> GridBFS(const vector<string>& grid, Point start, char wall) {
-	constexpr int INF = numeric_limits<int>::max();
+auto GridBFS(const std::vector<std::string>& grid, Point start, char wall_char) {
+	constexpr int INF = std::numeric_limits<int>::max();
 	int h = grid.size(), w = grid.front().size();
 	Point::set_range(h, w);
-	vector<vector<int>> result(h, vector<int>(w, INF));
-	if (grid[start.y][start.x] == wall) {
+
+	std::vector result(h, std::vector(w, INF));
+	if (grid[start.y][start.x] == wall_char) {
 		return result;
 	}
 	result[start.y][start.x] = 0;
-	queue<Point> q;
+	std::queue<Point> q;
 	q.push(start);
 	while (!q.empty()) {
 		auto now = q.front();
 		q.pop();
 		for (auto adj : now.adj4_in_range()) {
-			if (grid[adj.y][adj.x] != wall && result[adj.y][adj.x] == INF) {
+			if (grid[adj.y][adj.x] != wall_char && result[adj.y][adj.x] == INF) {
 				q.push(adj);
 				result[adj.y][adj.x] = result[now.y][now.x] + 1;
 			}
@@ -31,19 +31,20 @@ vector<vector<int>> GridBFS(const vector<string>& grid, Point start, char wall) 
 	}
 	return result;
 }
-vector<vector<int>> GridBFS(const vector<string>& grid, char start, char wall) {
+auto GridBFS(const std::vector<std::string>& grid, char start_char, char wall_char) {
 	Point::set_range(grid.size(), grid.front().size());
-	auto s = Point::find_one(grid, start);
-	assert(s);
-	return GridBFS(grid, *s, wall);
+	auto start = Point::find_one(grid, start_char);
+	assert(start);
+	return GridBFS(grid, *start, wall_char);
 }
-int GridBFS(const vector<string>& grid, Point start, Point goal, char wall) {
-	return GridBFS(grid, start, wall)[goal.y][goal.x];
+int GridBFS(const std::vector<std::string>& grid, Point start, Point goal, char wall_char) {
+	return GridBFS(grid, start, wall_char)[goal.y][goal.x];
 }
-int GridBFS(const vector<string>& grid, char start, char goal, char wall) {
-	assert(start != goal);
+int GridBFS(const std::vector<std::string>& grid, char start_char, char goal_char,
+            char wall_char) {
+	assert(start_char != goal_char);
 	Point::set_range(grid.size(), grid.front().size());
-	auto s = Point::find_one(grid, start), g = Point::find_one(grid, goal);
-	assert(s && g);
-	return GridBFS(grid, *s, *g, wall);
+	auto start = Point::find_one(grid, start_char), goal = Point::find_one(grid, goal_char);
+	assert(start && goal);
+	return GridBFS(grid, *start, *goal, wall_char);
 }
