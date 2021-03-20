29813497fcfc842507a7e5a63aceebbde17701e0
yuruhi <yuruhiya@gmail.com>
2021-03-20 18:11:34 +0900

    fix bug add Geometric::Vec2::pair remove using namespace std;

--- Geometry/Vec2.hpp
+++ Geometry/Vec2.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "./Geometric.hpp"
 #include <iostream>
+#include <utility>
 #include <cmath>
 
 namespace Geometric {
@@ -14,17 +15,17 @@ namespace Geometric {
 			return v1.y < v2.y;
 		}
 		static constexpr bool compare_xy(const Vec2& v1, const Vec2& v2) {
-			return make_pair(v1.x, v1.y) < make_pair(v2.x, v2.y);
+			return std::make_pair(v1.x, v1.y) < std::make_pair(v2.x, v2.y);
 		}
 		static constexpr bool compare_yx(const Vec2& v1, const Vec2& v2) {
-			return make_pair(v1.y, v1.x) < make_pair(v2.y, v2.x);
+			return std::make_pair(v1.y, v1.x) < std::make_pair(v2.y, v2.x);
 		}
 		static constexpr Vec2 zero() {
 			return Vec2(0, 0);
 		}
 		constexpr Vec2() : x(0), y(0) {}
 		constexpr Vec2(LD _x, LD _y) : x(_x), y(_y) {}
-		Vec2(LD rad) : x(cos(rad)), y(sin(rad)) {}
+		Vec2(LD rad) : x(std::cos(rad)), y(std::sin(rad)) {}
 		constexpr bool operator==(const Vec2& v) const {
 			return sgn(x - v.x) == 0 && sgn(y - v.y) == 0;
 		}
@@ -50,16 +51,16 @@ namespace Geometric {
 			return Vec2(*this) /= v;
 		}
 		constexpr Vec2 operator+(LD n) const {
-			return Vec2(*this) += Vec2(n, n);
+			return Vec2(*this) += n;
 		}
 		constexpr Vec2 operator-(LD n) const {
-			return Vec2(*this) -= Vec2(n, n);
+			return Vec2(*this) -= n;
 		}
 		constexpr Vec2 operator*(LD n) const {
-			return Vec2(*this) *= Vec2(n, n);
+			return Vec2(*this) *= n;
 		}
 		constexpr Vec2 operator/(LD n) const {
-			return Vec2(*this) /= Vec2(n, n);
+			return Vec2(*this) /= n;
 		}
 		constexpr Vec2& operator+=(const Vec2& v) {
 			x += v.x;
@@ -83,26 +84,29 @@ namespace Geometric {
 		}
 		constexpr Vec2& operator+=(LD n) {
 			x += n;
-			x += n;
+			y += n;
 			return *this;
 		}
 		constexpr Vec2& operator-=(LD n) {
 			x -= n;
-			x -= n;
+			y -= n;
 			return *this;
 		}
 		constexpr Vec2& operator*=(LD n) {
 			x *= n;
-			x *= n;
+			y *= n;
 			return *this;
 		}
 		constexpr Vec2& operator/=(LD n) {
 			x /= n;
-			x /= n;
+			y /= n;
 			return *this;
 		}
 		constexpr LD operator[](size_t i) const {
 			return i == 0 ? x : i == 1 ? y : 0;
+		}
+		constexpr std::pair<LD, LD> pair() const {
+			return {x, y};
 		}
 		LD manhattan(const Vec2& v) const {
 			return std::abs(x - v.x) + std::abs(y - v.y);
@@ -111,7 +115,7 @@ namespace Geometric {
 			return dot(*this);
 		}
 		LD length() const {
-			return sqrt(length_square());
+			return std::sqrt(length_square());
 		}
 		// 内積
 		constexpr LD dot(const Vec2& v) const {
@@ -127,12 +131,12 @@ namespace Geometric {
 		}
 		// 原点中心に rad 回転した座標
 		Vec2 rotation(LD rad) const {
-			LD c = cos(rad), s = sin(rad);
+			LD c = std::cos(rad), s = std::sin(rad);
 			return {x * c - y * s, x * s + y * c};
 		}
 		// 原点中心の円上に乗っているとしたときの偏角
 		LD angle() const {
-			return atan2(y, x);
+			return std::atan2(y, x);
 		}
 		// 正射影
 		Vec2 projection(const Line& l) const;
@@ -147,10 +151,10 @@ namespace Geometric {
 		constexpr Vec2 rotate270() const {
 			return {-y, x};
 		}
-		friend ostream& operator<<(ostream& os, const Vec2& v) {
+		friend std::ostream& operator<<(std::ostream& os, const Vec2& v) {
 			return os << '(' << v.x << ", " << v.y << ')';
 		}
-		friend istream& operator>>(istream& is, Vec2& v) {
+		friend std::istream& operator>>(std::istream& is, Vec2& v) {
 			return is >> v.x >> v.y;
 		}
 	};
